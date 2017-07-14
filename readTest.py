cd185a6aa4104253e1347d2b142cc0498e818c16
Eric <Eric.Syu@cubic.com>
2017-07-14 17:31:59 -0400

    edits

---
+++ readTest.py
@@ -0,0 +1,3 @@
+import serial, time
+
+ser1 = serial.Serial('/dev/ttyACM0', 9600)
--- sensorTest.py
+++ sensorTest.py
@@ -20,10 +20,10 @@ alarm = 0
 #Timing variables
 startCardTimer = False
 startIdleTimer = False
-startIdleTimer2 = False
+startGateTimer = False
 cardTimer = 0
 idleTimer = 0
-idleTimer2 = 0
+gateTimer = 0
 
 #stateStack = []
 #prevState = 0
@@ -51,6 +51,7 @@ print('Zones are as follows:')
 print('-----------------')
 print('| 1 | 2 | 3 | 4 |')
 print('-----------------')
+
 
 while Running:
 	time.sleep(0.1)
@@ -111,12 +112,12 @@ while Running:
 				elif shortFlag == 0:
 					shortFlag = 1
 
-	#turn on timer is safe flag 1
+	#turn on timer if safe flag 1
  	if shortFlag == 1:
-		startIdleTimer2 = True
+		startGateTimer = True
 	else:
-		idleTimer2 = 0
-		startIdleTimer2 = False
+		gateTimer = 0
+		startGateTimer = False
 
 #Alarm check
 	if alarm == 1:
@@ -125,9 +126,9 @@ while Running:
 			if startIdleTimer == True:
 				idleTimer = 0
 				startIdleTimer = False
-			if startIdleTimer2 == True:
-				idleTimer2 = 0
-				startIdleTimer2 = False
+			if startGateTimer == True:
+				gateTimer = 0
+				startGateTimer = False
 		else:
 			print("ALARM")
 
@@ -135,14 +136,14 @@ while Running:
 	if startCardTimer == True:
 		cardTimer += 1
 		# while loop runs every 0.1 seconds, 10 = roughly 1 sec(not accounting for program runtime)
-		if cardTimer >= 50:
+		if cardTimer >= 100:
 			startCardTimer = False
 			smartCard = 0
 			cardTimer = 0
 	#idleTimer
 	if startIdleTimer == True:
 		idleTimer += 1
-		if idleTimer >= 50:
+		if idleTimer >= 100:
 #			startIdleTimer = False
 #			sensors = [0,0,0,0,0,0]
 #			smartCard = 0
@@ -150,45 +151,52 @@ while Running:
 			currState = 1
 
 	#second timer for if gate is open too long
-	if startIdleTimer2 == True:
-		idleTimer2 += 1
-		if idleTimer2 >= 100:
+	if startGateTimer == True:
+		gateTimer += 1
+		if gateTimer >= 100:
 			alarm = 1
 
 #Set state rules
 
 	if currState == 1:
 		if sensors[0] == 1:
+			startIdleTimer = False
+			idleTimer = 0
 			currState = 2
-		elif sensors[1] == 1:
+		elif (sensors[1] or sensors[2] or sensors[3] == 1):
 			startIdleTimer = True
 	elif currState == 2:
 		startIdleTimer = True
 		if sensors[1] == 1:
+			startIdleTimer = False
+			idleTimer = 0
 			currState = 3
 	elif currState == 3:
+		startIdleTimer = True
 		if smartCard == 1:
+			startIdleTimer = False
+			idleTimer = 0
 			currState = 4
 	elif currState == 4:
-		idleTimer = 0
-		startIdleTimer = False
+		startIdleTimer = True
 		gate = 1
 		if sensors[2] == 1:
+			startIdleTimer = False
+			idleTimer = 0
 			currState = 5
 	elif currState == 5:
+		startGateTimer = True
 		if sensors[2] == 0 and shortFlag == 0:
+			startGateTimer = False
+			gateTimer = 0
 			currState = 6
-			idleTimer2 = 0
-			startIdleTimer2 = False
-		else:
-			startIdleTimer2 = True
 	elif currState == 6:
 		smartCard = 0
 		gate = 0
-		idleTimer = 0
-		if sensors[3] == 1:
+		if sensors[3] == 1 or shortFlag == 0:
 			currState = 7
 	elif currState == 7:
+		startIdleTimer = True
 		if sensors [3] == 0:
 			currState = 1
 
