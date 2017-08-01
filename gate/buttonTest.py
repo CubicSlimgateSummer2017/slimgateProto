2a45f6f26f603a7c04bc875db2c94a7c010123bc
Eric <Eric.Syu@cubic.com>
2017-08-01 13:30:11 +0000

    "first cleanup"

--- gate/buttonTest.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import Button
-
-button = Button(18)
-
-while (True):
-	if button.is_pressed:
-		print("asdfasdfdfasdfasdf")
--- gate/ledTest.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import LED
-from time import sleep
-
-led = LED(23)
-
-while True:
-	led.on()
